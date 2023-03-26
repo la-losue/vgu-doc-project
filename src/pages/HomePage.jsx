9a079f95b8fe2a0fd8e31d4f24df62a73d9aff0e
la-losue <lozzzomax17@gmail.com>
2023-03-26 22:25:43 +0300

    secondCommit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import  {Navigate} from 'react-router-dom'
+
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <Navigate to="/login"/>
+  )
+}
+
+export  {HomePage}
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <h1>Login</h1>
+
+        <p>
+            Or <Link to="/register">register</Link>
+        </p>
+    </div>
+  )
+}
+
+export {LoginPage}
